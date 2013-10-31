acd26e127a482f62da7123a7acd0dd3aacf71420
Bruno Koga <koga@brunokoga.com>
2013-10-31 10:39:14 -0200

    Initial project commit

---
+++ TVGuide/TVGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TVGAppDelegate.h
+//  TVGuide
+//
+//  Created by Bruno Koga on 10/31/13.
+//  Copyright (c) 2013 MobileAware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TVGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
